f39f3a8ecb9e9271e363a27bc8f8007d2b33bcba
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2026-03-19 09:42:33 -0300

    Primer actividad optativa - tres líneas de print en Python

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hola a todos, soy Noelia Ruiz")
+
+print("Saludos desde Python, me reservo la edad xD .. es broma, tengo 35 años")
+
+print("y vivo en la misma ciudad que vió nacer a Messi. Rosario, en Santa Fe, Argentina")
