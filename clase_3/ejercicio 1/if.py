4dffda533dfa0ecb273f78cda3b13082a8ae84d2
Ruiz Noelia <sagitrinidad@gmail.com>
2026-03-25 20:50:40 -0300

    Se resuelve ejercicio 1 - clase 3 - estructura básica if con captura de ejecución

---
+++ clase_3/ejercicio 1/if.py
@@ -0,0 +1,17 @@
+# Usando if o elif:Crear dos estructuras de código del tipo if/elif/else.
+# Ingresar los valores de las variables de forma manual.
+
+# Pasos básicos:
+# 1. Crear una variable y asignarle un valor.
+# 2. Crear la estructura de if/elif/else con lo visto.
+# 3. Agregar print() en cada instancia para ir probando cómo ingresa en cada decisión.
+# 4. Replicar la estructura de decisión.
+
+# Primera estructura if/elif/else
+edad = 25  # Paso 1: variable manual
+if edad < 18:
+	print("Menor de edad")
+elif edad <= 65:
+	print("Adulto")
+else:
+	print("Adulto mayor")
