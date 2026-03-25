4108bd7b8e2b3aec7069d701eada82fabe9ecd96
Ruiz Noelia <sagitrinidad@gmail.com>
2026-03-25 20:52:17 -0300

    Resolución de ejercicio 2 - clase 3 - con captura de ejecución

---
+++ clase_3/ejercicio 2/if.py
@@ -0,0 +1,17 @@
+# Usando if o elif:Crear dos estructuras de código del tipo if/elif/else.
+# Ingresar los valores de las variables de forma manual.
+
+# Pasos básicos:
+# 1. Crear una variable y asignarle un valor.
+# 2. Crear la estructura de if/elif/else con lo visto.
+# 3. Agregar print() en cada instancia para ir probando cómo ingresa en cada decisión.
+# 4. Replicar la estructura de decisión.
+
+# Segunda estructura if/elif/else
+nota2 = 5  # Otra variable manual
+if nota2 < 4:
+	print("Desaprobado")
+elif nota2 <= 7:
+	print("Aprobado")
+else:
+	print("Muy bien!")
