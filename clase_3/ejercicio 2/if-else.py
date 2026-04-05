1f948db2521d4a4b9cebaaada462fa342d7ade6e
Noelia Ruiz <ruiz.noeliaromina@gmail.com>
2026-04-05 16:52:26 -0300

    Ejercicio 2 de práctica, clase 3 - estructura if, elif y else con recetas

---
+++ clase_3/ejercicio 2/if-else.py
@@ -0,0 +1,18 @@
+# Usando if o elif:Crear dos estructuras de código del tipo if/elif/else. Ingresar los valores de las variables de forma manual.
+
+
+# Pasos básicos:
+# 1. Crear una variable y asignarle un valor.
+# 2. Crear la estructura de if/elif/else con lo visto.
+# 3. Agregar print() en cada instancia para ir probando cómo ingresa en cada decisión.
+# 4. Replicar la estructura de decisión.
+
+receta = "chocotorta"  # Variable con un valor asignado
+
+if receta == "tiramisú":
+    print("La receta es tiramisú.")
+elif receta == "chocotorta":
+    print("La receta es chocotorta.")
+else:
+    print("La receta no es ni tiramisú ni chocotorta.")
+    
