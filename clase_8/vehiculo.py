3b3e6bb0f58382bdd8ec6b1fa93c94d3c8008a7b
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2026-04-16 12:28:45 -0300

    Modificaciones en el código, se añaden métodos de consigna y se adjunta captura de ejecución del programa en terminal

--- clase_8/vehiculo.py
+++ clase_8/vehiculo.py
@@ -9,8 +9,27 @@ class vehiculo:
         self.año = año
         self.color = color
 
+# Un método para todos:
+# Con lo visto durante la clase, deberán armar un método como acelerar, con toda la información que crean necesaria. 
+# Luego, traten de generar dos métodos extras que crean que se puedan aplicar a vehículos.
+    
     def mostrar_info(self):
         return f"Vehículo: {self.marca} {self.modelo} ({self.año}), Color: {self.color}"
 
-auto = vehiculo("Toyota", "Corolla", 2020, "Rojo")
+    def acelerar(self, velocidad): # método principal de la consigna
+        self.velocidad = velocidad
+        return f"El {self.marca} {self.modelo} está acelerando a {self.velocidad} km/h."
+
+    def frenar(self): # primer método extra
+        self.velocidad = 0
+        return f"El {self.marca} {self.modelo} ha frenado."
+
+    def repintar(self, nuevo_color): # segundo método extra
+        self.color = nuevo_color
+        return f"El {self.marca} {self.modelo} ahora es de color {self.color}."
+
+auto = vehiculo("Citroën", "C3", 2015, "Violeta mauve") # se cambian datos para el ejercicio
 print(auto.mostrar_info())
+print(auto.acelerar(120))
+print(auto.repintar("Rosa malva")) # nuevo color
+print(auto.frenar())
