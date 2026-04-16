4af1ac2e2e82d3d5150431aa5d86d0235d138566
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2026-04-16 12:38:23 -0300

    Se suben ejemplos de herencia de clase con captura de ejecución del programa en terminal

--- clase_8/vehiculo.py
+++ clase_8/vehiculo.py
@@ -33,3 +33,36 @@ print(auto.mostrar_info())
 print(auto.acelerar(120))
 print(auto.repintar("Rosa malva")) # nuevo color
 print(auto.frenar())
+
+# Una herencia para todos:
+# Con lo visto durante la clase, deberán crear objetos Auto y Bicicleta que reutilizan los métodos del padre.
+
+class Auto(vehiculo): # hereda de vehiculo, agrega el atributo puertas y sobreescribe mostrar_info() para incluirlo
+    def __init__(self, marca, modelo, año, color, puertas):
+        super().__init__(marca, modelo, año, color)
+        self.puertas = puertas
+
+    def mostrar_info(self):
+        return f"{super().mostrar_info()}, Puertas: {self.puertas}"
+
+
+class Bicicleta(vehiculo):
+    def __init__(self, marca, modelo, año, color, tipo):
+        super().__init__(marca, modelo, año, color)
+        self.tipo = tipo
+
+    def mostrar_info(self):
+        return f"{super().mostrar_info()}, Tipo: {self.tipo}"
+
+# ACLARACIÖN: Ambas reutilizan los métodos acelerar() y frenar() del padre sin redefinirlos
+
+auto2 = Auto("Ford", "Focus", 2018, "Verde esmeralda", 4)
+bici = Bicicleta("Trek", "Marlin 5", 2022, "Celeste pastel", "Mountain Bike Silicon")
+print("-" * 50)
+print(auto2.mostrar_info())
+print(auto2.acelerar(100))
+print(auto2.frenar())
+print("-" * 50)
+print(bici.mostrar_info())
+print(bici.acelerar(30))
+print(bici.frenar())
